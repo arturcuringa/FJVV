f12118480163059096d54beab07a1de7375ebde3
Artur <arturmcuringa@gmail.com>
2018-08-29 09:58:48 -0300

    Add STMT

--- src/recursive-descent.c
+++ src/recursive-descent.c
@@ -6,7 +6,43 @@ enum token tok;
 void error() { printf("Parsing error on line %d and col %d!\n", line, col); }
 void advance() { tok = yylex(); }
 void eat(enum token t) { if (tok == t) advance(); else error(); }
+void STMT();
+void IDLESSSTMT();
 
+
+void POSTLABELESSSTMT(){
+	switch (tok){
+		case ATTR_SIGN:
+			ATTRSTMT();
+			break;
+		case LPAREN:
+			PROCSTMT();
+			break;
+		default:
+			error();
+	
+	}
+}
+
+void IDLESSSTMT(){
+	switch (tok){
+		case STOP:
+			STOPSTMT();
+			break;
+		case PUT:
+		case GET:
+			IOSTMT();
+			break;
+		case GOTO:
+		case IF:
+		case LOOP:
+		case EXITWHEN:
+			CONTROLSTMT();
+		default:
+			erorr();
+	}
+
+}
 void STARTTOK() {
 	switch (tok) {
 		case START:
@@ -24,6 +60,53 @@ void TERMINATORTOK() {
 			break;
 		default:
 			printf("missing ;\n");
+	}
+}
+
+void POSTLABELSTMT (){
+	switch (tok){
+		case COLON:
+			eat(COLON);
+			STMT();
+			break;
+		case ATTR_SIGN: 
+			ATTRSTMT();
+			break;
+		case LPAREN:
+			PROCSTMT();
+			break;
+		default:
+			error();
+	}
+}
+
+void LABELLESSTMT() {
+	switch (tok){
+		case IDENTIFIER:
+			eat(IDENTIFIER);
+			POSTLABELESSSTMT();
+			break;
+		default:
+			error();
+	}
+}
+
+void STMT() {
+	switch (tok) {
+		case IDENTIFIER:
+			LABELLESSTMT();
+			break;
+		case STOP:
+		case IF:
+		case GOTO:
+		case LOOP:
+		case EXITWHEN:
+		case PUT:
+		case GET:
+			IDLESSSTMT();
+			break;
+		default:
+			error();
 	}
 }
 
@@ -55,12 +138,13 @@ void DATATYPETOK() {
 	eat(DATATYPE);
 }
 
+
 void ARRAYTOK(){
 	switch(tok){
 		case ARRAY:
 			eat(ARRAY);
 			break;
-		deafult:
+		default:
 			printf("Missing ARRAY declaration!\n");
 	}
 }
@@ -123,6 +207,20 @@ void PROCDECLLIST() {
 
 }
 
+
+
+void LABELSTMT() {
+	switch (tok){
+		case IDENTIFIER:
+			eat(IDENTIFIER);
+			POSTLABELSTMT();
+			break;
+		default:
+			error();
+	}
+}
+
+
 void SUPERSTMT() {
 	switch (tok) {
 		case IDENTIFIER:
@@ -134,7 +232,7 @@ void SUPERSTMT() {
 		// case LOOP
 		// case EXTIWHEN
 		// case STOP
-			IDLESSSTMT();
+			//IDLESSSTMT();
 			break;
 		default:
 			printf("Something happened\n");
