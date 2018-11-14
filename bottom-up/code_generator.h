90bf48ced81fa896cd14e53565f5030d29709cf7
Artur <arturmcuringa@gmail.com>
2018-11-14 09:32:47 -0300

    Add loop stmt generator

--- bottom-up/code_generator.h
+++ bottom-up/code_generator.h
@@ -6,12 +6,17 @@
 
 
 extern unsigned int if_counter;
+extern unsigned int loop_counter;
+
 void generateCode(const Node& node);
 void generateCode(const Program& p);
-void generateCode(const std::shared_ptr<Stmt>& stmt);
+void generateCode(const std::shared_ptr<Stmt>& stmt, int  loop_counter);
 
 template <class T>
 void generateCode(const std::vector<T>& list);
+
+template <>
+void generateCode(const std::vector<std::shared_ptr<Stmt>>& list);
 
 std::string parseExpr(const std::shared_ptr<Expr>& expr);
 
